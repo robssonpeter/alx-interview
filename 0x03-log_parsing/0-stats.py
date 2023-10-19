051fe8d1eb7df2ec5768d8e523d9aeb354a5e97f
Peter Mgembe <peterrobsson@gmail.com>
2023-10-19 15:29:40 +0300

    solving question 0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,46 +1,40 @@
 #!/usr/bin/python3
-""" The script to count the number of occurances of status codes """
+""" The script to count the number of occurrences of status codes """
 import sys
 import re
 
 
-std_in = sys.stdin
-pattern = ''
+def print_statistics(file, status_code_counts):
+    print(f"File size: {len(file)}")
+    for key in sorted(status_code_counts.keys()):
+        print(f"{key}: {status_code_counts[key]}")
 
-part1 = '^[0-9]*.[0-9]*.[0-9]*.[0-9]* - '
-part2 = '[[0-9]*-[0-9]*-[0-9]* [0-9]*:[0-9]*:[0-9]*.[0-9]*] '
-part3 = '\"GET /projects/260 HTTP/1\\.1\" [0-9]* [0-9]'
+
+ip = r'^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+ - '
+timestamp = r'\[[0-9]+-[0-9]+-[0-9]+ [0-9]+:[0-9]+:[0-9]+\.[0-9]+\] '
+http_request = r'"GET /projects/260 HTTP/1\.1" [0-9]+ [0-9]+'
+
 file = ''
 index = 1
 allowed_codes = [200, 301, 400, 401, 403, 404, 405, 500]
-number_of_codes = {
+number_of_codes = {}
 
-}
-
-joined_pattern = part1+part2+part3
 try:
-    for line in std_in:
-        file = file+line
+    for line in sys.stdin:
+        file += line
         lst = line.split(' ')
         status_code = int(lst[-2])
-        found = re.search(joined_pattern, line)
+        found = re.search(ip + timestamp + http_request, line)
         if status_code not in allowed_codes or found is None:
             index += 1
             continue
-        keys = number_of_codes.keys()
-
-        if status_code in keys:
+        if status_code in number_of_codes:
             number_of_codes[status_code] += 1
         else:
             number_of_codes[status_code] = 1
-
         if index % 10 == 0:
-            print(f"File size: {len(file)}")
-            for key in sorted(number_of_codes.keys()):
-                print(f"{key}: {number_of_codes[key]}")
+            print_statistics(file, number_of_codes)
         index += 1
+
 except KeyboardInterrupt:
-    print
-    print(f"File size: {len(file)}")
-    for key in sorted(number_of_codes.keys()):
-        print(f"{key}: {number_of_codes[key]}")
+    print_statistics(file, number_of_codes)
